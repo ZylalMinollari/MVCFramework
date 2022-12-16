8b27cb6bd021f1926848b1e3f1f6a1f03a3e1fe9
ZylalMinollari <zminollari@gmail.com>
2022-12-16 15:43:37 +0100

    Implement controller in MVC framework

---
+++ core/Controller.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\core;
+
+use app\core\Aplication;
+
+/**
+ * Summary of Controller
+ * @author ZYLAL
+ * @copyright (c) 2022
+ */
+
+class Controller
+{
+    public string $layout = 'main';
+    public function setLayout($layout) {
+        $this->layout = $layout;
+    }
+    public function render($view,$params=[]) {
+        return Aplication::$app->router->renderView($view, $params);
+    }
+}
--- public/index.php
+++ public/index.php
@@ -2,15 +2,20 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use app\controllers\AuthController;
+use app\controllers\SiteController;
 use app\core\Aplication;
 
 $app = new Aplication(dirname(__DIR__));
 
-$app->router->get('/', 'home');
+$app->router->get('/', [SiteController::class, 'home']);
 
-$app->router->get('/contact', 'contact');
-$app->router->post('/contact', function () {
-    return 'hendeliing';
-});
+$app->router->get('/contact', [SiteController::class, 'contact']);
+$app->router->post('/contact', [SiteController::class, 'handleContact']);
+
+$app->router->get('/login', [AuthController::class, 'login']);
+$app->router->post('/login', [AuthController::class, 'login']);
+$app->router->get('/register', [AuthController::class, 'register']);
+$app->router->post('/register', [AuthController::class, 'register']);
 
 $app->run();
